e332942adb2cbfd10791ec01fefa82bdc034451a
crepic21 <crepic21@gmail.com>
2023-02-05 11:15:43 +0100

    added string and bytes encoding and decoding script

---
+++ 07. Network_Automation_with_Telnet/01_str_and_bytes.py
@@ -0,0 +1,16 @@
+s1 = 'abc'
+b1 = s1.encode('utf-8')
+print(type(s1))
+print(type(b1))
+print(b1)
+for b in b1:
+    print(b)
+
+s2 = 'ハッキング' # japanese for hacking
+b2 = s2.encode()
+print(b2)
+for b in b2:
+    print(b)
+
+s2_decoded = b2.decode()
+print(s2_decoded)
